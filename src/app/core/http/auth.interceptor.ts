e4d68359285d68351a99431351866f32ab4ad2d5
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-16 01:23:46 +0300

    3.4 handle 401 with refresh and retry request

--- src/app/core/http/auth.interceptor.ts
+++ src/app/core/http/auth.interceptor.ts
@@ -1,5 +1,8 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, switchMap } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 import { inject } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 import { AuthStateService } from '../auth/auth-state.service';
 
 
@@ -7,22 +10,39 @@ const shouldSkipAuth = (url : string) =>
     url.includes('/auth/login') || url.includes('/auth/refresh');
 
 
+
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
 
   if(shouldSkipAuth(req.url)){
     return next(req);
   }
-
+  const authService = inject(AuthService);
   const authState = inject(AuthStateService);
   const token = authState.accessToken;
-  if (!token){
-      return next(req);
-  }
-
-  const authReq = req.clone({
+  const tokenToSend = token?
+    req.clone({
     setHeaders:{
       Authorization: `Bearer ${token}`,
     },
-  });
-  return next(authReq);
-}
+  }): req;
+  return next(tokenToSend).pipe(
+    catchError( (error : unknown)=>{
+      if(error instanceof HttpErrorResponse && error.status === 401){
+        return authService.refresh().pipe(
+          switchMap( () => {
+            const newToken = authState.accessToken;
+            const retryReq = newToken ? req.clone({setHeaders:{Authorization: `Bearer ${newToken}`,},}) : req;
+            return next(retryReq);
+
+          }),
+          catchError((refreshErr) => {authState.logout();return throwError(() => refreshErr)})
+        )
+      }
+      return throwError(()=> error);
+
+    }
+    ));
+
+
+};
