acb039b1b111a1998cd8c25bccbece1fd48bd1c2
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-16 14:24:34 +0300

    3.5.1.2.1 login form group setup

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone:true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+form = new FormGroup({
+  username: new FormControl ('',Validators.required),
+  password: new FormControl ('',Validators.required)
+});
 }
