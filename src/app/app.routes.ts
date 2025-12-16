600f5e8d1b8ef0c49305079bdd6360b539be2622
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-16 13:20:14 +0300

    3.5.1.1 login route + router outlet + login skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login').then((m) => m.Login),
+  },
+  { path: '**', redirectTo: 'login' },
+];
