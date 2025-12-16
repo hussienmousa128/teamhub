e5190511070427f10f479fddc99bf8f2408e39cd
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-16 13:45:44 +0300

    3.5.1.1.a remove unused login import from routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { Login } from './features/auth/pages/login/login';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./features/auth/pages/login/login').then((m) => m.Login),
-  },
+  {path: 'login', component : Login},
   { path: '**', redirectTo: 'login' },
 ];
