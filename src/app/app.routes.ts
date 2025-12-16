f589a58dd7ee6db0810b35a3e9fed18a4009d26d
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-16 19:51:32 +0300

    3.5.5.a users placeholder route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
+import { Users } from './features/users/pages/users/users';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   {path: 'login', component : Login},
+  {path: 'app/users' , component : Users},
   { path: '**', redirectTo: 'login' },
 ];
